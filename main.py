718dd7cc5b41f62381024be67eb27d58c10ff393
TDzenis <Tomass.Dzenis@outlook.com>
2019-12-28 21:12:41 +0000

    Cleaned and optimised the code into functions Added MySQL database login

--- main.py
+++ main.py
@@ -1,53 +1,79 @@
 from requests import get
 from bs4 import BeautifulSoup
 import time
+import mysql.connector
+from mysql.connector import errorcode
 
-start = time.time() # Timer to measure programs performance
+url = 'https://www.currys.co.uk/product.xml'  # Main product file
+DB_NAME = 'products'
 
-url = 'https://www.currys.co.uk/product.xml' # Main product file
-response = get(url)
 
-html_soup = BeautifulSoup(response.text, 'html.parser')
-type(html_soup)
+def get_html(linkToWebsite):
+    response = get(linkToWebsite)
+    html = BeautifulSoup(response.text, 'html.parser')
+    return html
 
-products = html_soup.find_all('loc')
+
+def connect_db(usr, pwd, hst):
+    cnx = mysql.connector.connect(user=usr, password=pwd,
+                                  host=hst)
+    return cnx
+
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+        print("DB connected succesfuly")
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+
+
+
+start = time.time()  # Timer to measure programs performance
+
+products = get_html(url).find_all('loc')
 print("Product count: ", len(products))  # Total link count in the main product file
 print("\n****************************************")
 
-counter = 0 # Loops through the products on the main product file to get the sku, name and price
+counter = 0  # Loops through the products on the main product file to get the sku, name and price
 for i in products:
     counter += 1
-    if counter > 10:
+    if counter > 1:
         break
-    url2 = i.text
-    response = get(url2) # Loads the product page ready for scraping
-
-    html_soup2 = BeautifulSoup(response.text, 'html.parser')
-    type(html_soup2)
-
-    productSku = html_soup2.find(attrs={"class": "prd-code"}) # Finds the SKU (product code)
+    productSku = get_html(i.text).find(attrs={"class": "prd-code"})  # Finds the SKU (product code)
+    productName = get_html(i.text).find(attrs={"class": "page-title nosp"})  # Finds the name of the item
+    productPrice = get_html(i.text).find(attrs={"data-key": "current-price"})  # Finds the price of the item
     try:
         print(productSku.text)
-    except:
-        #print("Can't find a SKU number!")
-        continue
-
-    productName = html_soup2.find(attrs={"class":"page-title nosp"}) # Finds the name of the item
-    try:
         print(productName.text.replace('\n', ' ')[1:-1])
-    except:
-        #print("Cant find the product name!")
-        continue
-
-    productPrice = html_soup2.find(attrs={"data-key": "current-price"}) # Finds the price of the item
-    try:
         print(productPrice.text)
     except:
-        #print("Can't find the price!")
         continue
 
     print("++++++++++++++++++++++++++++++++++++++++++++")
 
+    # time.sleep(5) # Sleep timer to prevent the website blocking the scraping.
+
 end = time.time()
 
-print(end - start)
+print(round(end - start, 2), 'seconds')
+
+
+cnx = connect_db('USERNAME', 'PASSWORD', 'HOST')
+
+
+try:
+    cnx.cursor().execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cnx.cursor())
+        print("Database {} created successfully.".format(DB_NAME))
+        cnx.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
+
+cnx.close()
